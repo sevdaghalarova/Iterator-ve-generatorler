df3616dc4608dafe152ba4db0ca336dc685da0e9
sevdaghalarova <51816139+sevdaghalarova@users.noreply.github.com>
2020-10-15 00:54:54 +0400

    ornek code

---
+++ generator_ornek.py
@@ -0,0 +1,14 @@
+def fibonacci():
+    a=1
+    b=1
+    yield a
+    yield b
+    while True:
+        a,b=b,a+b
+        yield b
+
+for i in fibonacci():
+    if i>1000:
+        break
+    else:
+        print(i)
